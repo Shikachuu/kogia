d95d19ef5273e9672d1031509275f682c3c608a2
Máté Czékus <mate@picloud.hu>
2026-04-03 21:59:44 +0200

    feat: add custom swagger codegen
    Signed-off-by: Máté Czékus <mate@picloud.hu>

---
+++ internal/api/gen/gen.go
@@ -0,0 +1,5 @@
+// Package gen contains generated route definitions for the Docker Engine API.
+// Do not edit routes.go manually. Regenerate with: mise run gogen
+package gen
+
+//go:generate go run ../../../hack/gen-routes
